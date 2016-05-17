715292c3f632905d09189cd03dc5c13fe92a79b5
leslie3141 <leslie3141@gmail.com>
2016-05-18 09:44:00 +1000

    Made resources smaller to make them load faster, and added score calculation stuff

--- src/Tile.java
+++ src/Tile.java
@@ -1,24 +1,27 @@
 /**
  * Created by brad on 9/05/16.
  */
-public class Tile
-{
+public class Tile {
     public static final char WALL = 'w';
     public static final char EMPTY = 'e';
     public static final char START = 's';
     public static final char FINISH = 'f';
     public static final char ITEM = 'i';
     
-    char value;
+    private char value;
+    private int score;
 
-    public Tile(char ch)
-    {
-        value = ch;
+    public Tile(char value, int score) {
+        this.value = value;
+        this.score = score;
     }
 
-    public char getValue()
-    {
+    public char getValue() {
         return value;
+    }
+    
+    public int getScore() {
+        return score;
     }
 
 }

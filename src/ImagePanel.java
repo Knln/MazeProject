54c9bffb84251d1498b0899ec795ba07411f4f4e
James <james.chowdhury1994@gmail.com>
2016-05-20 12:21:41 +0000

    ImagePanel.java edited online with Bitbucket

--- src/ImagePanel.java
+++ src/ImagePanel.java
@@ -1,37 +1,66 @@
 import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
-
 import javax.imageio.ImageIO;
 import javax.swing.JPanel;
 
-public class BackgroundPanel extends JPanel {
-    
-    /**
-     * Some serial ID
-     */
-    private static final long serialVersionUID = -9139266324799167727L;
-    
-    private String backgroundImage;
-    
-    public BackgroundPanel() {
-        super();
+public class ImagePanel extends JPanel{
+	
+	/**
+	 * Some serial ID
+	 */
+	private static final long serialVersionUID = 3273612744300406780L;
+	
+	private BufferedImage backgroundImage;	
+	private BufferedImage foregroundImage;
+	private int width;
+	private int height;
+	private boolean isForegroundVisible;
+
+    public ImagePanel(String backgroundIcon, String foregroundIcon, int height, int width, boolean isForegroundVisible) {
+        
+    	// load the background image
+    	if (backgroundIcon != null) {
+    		try {
+    			backgroundImage = ImageIO.read(new File(backgroundIcon));
+    		} catch (IOException e) {
+        		System.out.println("Error: " + backgroundImage + " not found");
+        	}
+    	}
+        
+        //load the foreground image
+        if (foregroundIcon != null) {
+    		try {
+    			foregroundImage = ImageIO.read(new File(foregroundIcon));
+    		} catch (IOException e) {
+    			System.out.println("Error: " + foregroundImage + " not found");
+    		}
+    	}
+        
+        this.isForegroundVisible = isForegroundVisible;
+        this.height = height;
+        this.width = width;      
     }
-    
-    public void setBackgroundImage(String image) {
-        backgroundImage = image;
-    }
-    
+
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
-        if (backgroundImage != null) {
-            try {
-                g.drawImage(ImageIO.read(new File(backgroundImage)), 0, 0, null);
-            } catch (IOException e) {
-                System.out.println("Error: " + backgroundImage + " not found");
-            }
+        Graphics2D g2d = (Graphics2D) g.create();
+        if (width * height == 0){
+        	//if height and width are unreasonable (i.e. 0) use default image size
+        	g2d.drawImage(backgroundImage, 0, 0, this);
+        	if (this.isForegroundVisible == true){
+        		g2d.drawImage(foregroundImage, 0, 0, this);
+        	}
+        } else {
+        	//use designated width and height
+        	g2d.drawImage(backgroundImage, 0, 0, width, height, this);
+        	if (this.isForegroundVisible == true){
+        		g2d.drawImage(foregroundImage, 0, 0, width, height, this);
+        	}
         }
+        g2d.dispose();
     }
-
 }
